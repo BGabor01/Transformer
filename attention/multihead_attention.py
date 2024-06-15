5c300dedb962bdef903a084ee7550e787b2963c1
BGabor01 <busnes14@gmail.com>
2024-06-15 10:05:57 +0200

    multihead_attn: docstrings

--- attention/multihead_attention.py
+++ attention/multihead_attention.py
@@ -7,9 +7,25 @@ from scaled_dot_product import ScaledDotProductAttention
 
 
 class MultiHeadAttention(nn.Module):
+    """Multi-head attention mechanism"""
+
     def __init__(self, n_heads: int, model_dim: int) -> None:
+        """
+        Args:
+        n_heads (int): Number of attention heads.
+        key_vector_dim (int): Dimension of the key vectors for each head.
+        value_vector_dim (int): Dimension of the value vectors for each head.
+        model_dim (int): Dimension of the model.
+        weight_qs (nn.Linear): Linear layer to project the query vectors.
+        weight_ks (nn.Linear): Linear layer to project the key vectors.
+        weight_vs (nn.Linear): Linear layer to project the value vectors.
+        output_projection (nn.Linear): Linear layer for the final output projection.
+        attention (ScaledDotProductAttention): Scaled dot-product attention mechanism.
+        """
         super().__init__()
-        assert model_dim % n_heads == 0
+        assert (
+            model_dim % n_heads == 0
+        ), "Model dimension must be divisible by the number of heads."
 
         self.n_heads = n_heads
         self.key_vector_dim = model_dim // n_heads
@@ -30,6 +46,18 @@ class MultiHeadAttention(nn.Module):
         value: torch.Tensor,
         mask: Optional[torch.Tensor] = None,
     ):
+        """
+        Forward pass for multi-head attention.
+
+        Args:
+            query (torch.Tensor): Query tensor of shape (batch_size, seq_length, model_dim).
+            key (torch.Tensor): Key tensor of shape (batch_size, seq_length, model_dim).
+            value (torch.Tensor): Value tensor of shape (batch_size, seq_length, model_dim).
+            mask (Optional[torch.Tensor]): Mask tensor of shape (batch_size, seq_length, seq_length). Default is None.
+
+        Returns:
+            torch.Tensor: Output tensor after applying multi-head attention.
+        """
         batch_size = query.size(0)
         query = (
             self.weight_qs(query)
