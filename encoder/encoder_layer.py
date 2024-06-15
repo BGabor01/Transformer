4dea9d0ee584087d8d98e23b3cadf6f54e7d9b44
BGabor01 <busnes14@gmail.com>
2024-06-15 15:56:21 +0200

    ecnd_layer: updated

--- encoder/encoder_layer.py
+++ encoder/encoder_layer.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 from attention import MultiHeadAttention
-from .feed_forward import FeedForward
+from neural_network import FeedForward
 
 
 class EncoderLayer(nn.Module):
