e7dcb8797d12fdcac02a1e9b401dce67a49b8077
BGabor01 <busnes14@gmail.com>
2024-06-16 19:42:21 +0200

    test_embed: input embedding tests

---
+++ tests/test_embedding.py
@@ -0,0 +1,32 @@
+import pytest
+import torch
+
+from model.embedding import InputEmbedding
+
+
+class TestEnbedding:
+
+    @pytest.fixture
+    def embedding(self):
+        vocab_size = 100
+        embed_dim = 50
+        return InputEmbedding(vocab_size, embed_dim)
+
+    @pytest.mark.smoke
+    def test_embedding_values(self, embedding):
+        input_tensor = torch.tensor([[1, 2, 3], [4, 5, 6]])
+        output_tensor = embedding(input_tensor)
+
+        expected_shape = (2, 3, embedding.embeddings.embedding_dim)
+        assert (
+            output_tensor.shape == expected_shape
+        ), f"Expected shape {expected_shape}, but got {output_tensor.shape}"
+
+    @pytest.mark.slow
+    def test_embedding_grad(self, embedding):
+        input_tensor = torch.tensor([[1, 2, 3], [4, 5, 6]], dtype=torch.long)
+        output_tensor = embedding(input_tensor)
+        output_tensor.mean().backward()
+        assert (
+            embedding.embeddings.weight.grad is not None
+        ), "Gradients are not computed"
