0eabeed5162152b8e0e12898d899c56cb2de7994
BGabor01 <busnes14@gmail.com>
2024-06-16 14:24:44 +0200

    enput_emb: Added droput arg

--- embedding/input_embedding.py
+++ embedding/input_embedding.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import torch
 import torch.nn as nn
 
@@ -7,7 +8,9 @@ from .positional_encoding import PositionalEncoding
 class InputEmbedding(nn.Module):
     """Embedding layer with positional encoding for input sequences."""
 
-    def __init__(self, vocab_size: int, embedding_dim: int) -> None:
+    def __init__(
+        self, vocab_size: int, embedding_dim: int, dropout: Optional[float] = 0.1
+    ) -> None:
         """
         Args:
             vocab_size (int): Size of the vocabulary.
@@ -15,7 +18,7 @@ class InputEmbedding(nn.Module):
         """
         super().__init__()
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        self.pos_encode = PositionalEncoding(embedding_dim)
+        self.pos_encode = PositionalEncoding(embedding_dim, dropout)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         """
