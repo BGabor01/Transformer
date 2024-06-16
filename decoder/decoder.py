9b4d7f4211ab04ec6753c511cb925fb771144cde
BGabor01 <busnes14@gmail.com>
2024-06-16 14:23:07 +0200

    decoder: Refactored using decoder block

--- decoder/decoder.py
+++ decoder/decoder.py
@@ -1,64 +1,41 @@
 from typing import Optional
-import torch
+
 import torch.nn as nn
-from .decoder_layer import DecoderLayer
+
 from embedding import InputEmbedding
+from .decoder_block import DecoderBlock
 
 
 class Decoder(nn.Module):
-    """Decoder consisting of multiple decoder layers"""
-
     def __init__(
         self,
-        model_dim: int,
         vocab_size: int,
+        model_dim: int,
+        key_dim: int,
+        value_dim: int,
+        max_len: int,
         n_heads: int,
-        n_layer: int,
-        ff_dim: int,
+        n_layers: int,
         dropout: Optional[float] = 0.1,
     ) -> None:
-        """
-        Initializes the Encoder module.
-
-        Args:
-        model_dim (int): Dimension of the model.
-        vocab_size (int): Size of the vocabulary.
-        n_heads (int): Number of attention heads.
-        n_layer (int): Number of decoder layers.
-        ff_dim (int): Dimension of the feed-forward layer.
-        dropout (Optional[float]): Dropout rate. Default is 0.1.
-        """
         super().__init__()
-        self.target_embedding = InputEmbedding(vocab_size, model_dim)
-        self.layers = nn.ModuleList(
-            [DecoderLayer(n_heads, model_dim, ff_dim, dropout) for _ in range(n_layer)]
-        )
-        self.norm = nn.LayerNorm(model_dim)
-        self.dropout = nn.Dropout(dropout)
+        self.input_embedding = InputEmbedding(vocab_size, model_dim, dropout)
+        transformer_blocks = [
+            DecoderBlock(key_dim, value_dim, model_dim, n_heads, max_len, dropout)
+            for _ in range(n_layers)
+        ]
+        self.transformer_blocks = nn.Sequential(*transformer_blocks)
+        self.norm_layer = nn.LayerNorm(model_dim)
+        self.output_layer = nn.Linear(model_dim, vocab_size)
 
     def forward(
-        self,
-        target: torch.Tensor,
-        encoder_output: torch.Tensor,
-        target_mask: Optional[torch.Tensor] = None,
-        encoder_mask: Optional[torch.Tensor] = None,
-    ) -> torch.Tensor:
-        """
-        Forward pass for the decoder.
+        self, encoder_output, decoder_input, encoder_mask=None, decoder_mask=None
+    ):
+        output = self.input_embedding(decoder_input)
+        for block in self.transformer_blocks:
+            output = block(encoder_output, output, encoder_mask, decoder_mask)
 
-        Args:
-            target (torch.Tensor): Target tensor of shape `[batch_size, seq_len]`.
-            encoder_output (torch.Tensor): Encoder output tensor of shape `[batch_size, seq_len, model_dim]`.
-            target_mask (Optional[torch.Tensor]): Target mask tensor of shape `[batch_size, 1, target_len, target_len]`. Default is None.
-            encoder_mask (Optional[torch.Tensor]): Encoder mask tensor of shape `[batch_size, 1, 1, mem_len]`. Default is None.
-
-        Returns:
-            torch.Tensor: Output tensor of shape `[batch_size, seq_len, model_dim]`.
-        """
-
-        target = self.target_embedding(target)
-
-        for layer in self.layers:
-            target = layer(target, encoder_output, target_mask, encoder_mask)
-
-        return self.norm(target)
+        output = self.norm_layer(output)
+        # many-to-many
+        output = self.output_layer(output)
+        return output
